e2e43bdcc1faa631e1bd3804d3d4bad8f78626f6
dmitry741 <dmitrypavlov74@gmail.com>
2017-06-21 15:31:43 +0300

    update packages
    new computer, ridan

--- WebStore1/Models/User.cs
+++ WebStore1/Models/User.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Entity.ModelConfiguration;
 
 
 namespace WebStore1.Models
